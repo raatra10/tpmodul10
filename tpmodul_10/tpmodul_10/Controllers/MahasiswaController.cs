5b2739896f21a09ed073722852d155856fc77136
raatra10 <rafi180350@gmail.com>
2025-04-27 20:26:48 +0700

    menambahkan API mahasiswa

---
+++ tpmodul_10/tpmodul_10/Controllers/MahasiswaController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul_10_103022300123.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswas = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Rafi A.R", Nim = "1302000001" },
+            new Mahasiswa { Nama = "Stephen curry", Nim = "1302000002" },
+            new Mahasiswa { Nama = "Jecksmock", Nim = "1302000003" },
+            new Mahasiswa { Nama = "Merzen", Nim = "1302000004" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> Get()
+        {
+            return mahasiswas;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Mahasiswa> Get(int id)
+        {
+            if (id >= 0 && id < mahasiswas.Count)
+            {
+                return mahasiswas[id];
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Mahasiswa value)
+        {
+            mahasiswas.Add(value);
+            return CreatedAtAction(nameof(Get), new { id = mahasiswas.Count - 1 }, value);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (id >= 0 && id < mahasiswas.Count)
+            {
+                mahasiswas.RemoveAt(id);
+                return NoContent();
+            }
+            return NotFound();
+        }
+    }
+
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
